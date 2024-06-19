0d1a3ea275cb39e90cdf1ef8a0530aadcc4a735f
Damien Caliste <dcaliste@free.fr>
2024-06-19 14:20:59 +0200

    Adjust to the consumption column.

--- at.py
+++ at.py
@@ -6,7 +6,7 @@ import datetime
 
 d = numpy.loadtxt("history" if len(sys.argv) == 3 else sys.argv[3] )
 
-for (t, v) in d:
+for (t, v, c) in d:
     dt = datetime.datetime.fromtimestamp(t)
     if (dt.timetuple().tm_yday == int(sys.argv[1]) and dt.timetuple().tm_year == int(sys.argv[2])):
         print(dt.hour + dt.minute / 60 + dt.second / 3600, v)
