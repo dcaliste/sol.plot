965e8ca3d153252873cd247fad05fbf9de6daf7f
Damien Caliste <dcaliste@free.fr>
2024-06-26 10:41:05 +0200

    Allow to compute consumption for a range.

--- conso.py
+++ conso.py
@@ -6,16 +6,29 @@ import datetime
 
 d = numpy.loadtxt("history")
 
-f = int(datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").timestamp())
-t = f + 24 * 3600
+def conso(d, f, t = None):
+  if t is None:
+    t = f + 24 * 3600
 
-mask = numpy.logical_and(d[:, 0] >= f, d[:, 0] < t)
-times = d[mask, 0]
-sprod = d[mask, 1]
-conso = d[mask, 2]
+  mask = numpy.logical_and(d[:, 0] >= f, d[:, 0] < t)
+  times = d[mask, 0]
+  sprod = d[mask, 1]
+  conso = d[mask, 2]
 
-save = numpy.trapz(sprod - numpy.where(conso < 0, 0, conso), times) / 3600000
-prod = numpy.trapz(sprod, times) / 3600000
-edfs = numpy.trapz(numpy.where(conso < 0, -conso, 0), times) / 3600000
+  save = numpy.trapz(sprod - numpy.where(conso < 0, 0, conso), times) / 3600000
+  prod = numpy.trapz(sprod, times) / 3600000
+  edfs = numpy.trapz(numpy.where(conso < 0, -conso, 0), times) / 3600000
 
-print((save, prod, edfs)[int(sys.argv[2])])
+  return save, prod, edfs
+
+if len(sys.argv) > 1:
+  f = int(datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").timestamp())
+  print(conso(d, f)[int(sys.argv[2])])
+else:
+  s = datetime.datetime.fromtimestamp(d[0, 0], tz = datetime.timezone.utc)
+  f = datetime.datetime(s.year, s.month, s.day + 1, tzinfo = datetime.timezone.utc).timestamp()
+  while f < d[-1, 0] - 24 * 3600:
+    vals = conso(d, f)
+    if vals[2] > 0.:
+      print(f, *vals)
+    f += 24 * 3600
