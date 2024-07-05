2d16f79b20a9acba293ac974fa2ed8ef2a1ddbbf
Damien Caliste <dcaliste@free.fr>
2024-07-05 11:50:03 +0200

    Add a colour map plot for consumption.

--- split.py
+++ split.py
@@ -12,4 +12,4 @@ for (t, v, c) in d:
     if prev is not None and prev != dt.day:
         print()
     prev = dt.day
-    print(dt.timetuple().tm_yday, "%02d:%02d:%02d" % (dt.hour, dt.minute, dt.second), v, dt.timetuple().tm_year)
+    print(dt.timetuple().tm_yday, "%02d:%02d:%02d" % (dt.hour, dt.minute, dt.second), v, v - c, dt.timetuple().tm_year)
