c564efcf7dcbf834ce2ed2c86e4db3bbdec0c5b4
Damien Caliste <dcaliste@free.fr>
2024-10-24 16:49:03 +0200

    Ignore incomplete day when integrating.

--- conso.py
+++ conso.py
@@ -6,7 +6,7 @@ import datetime
 
 d = numpy.loadtxt("history")
 
-def conso(d, f, t = None):
+def conso(d, f, t = None, partial = False):
   if t is None:
     t = f + 24 * 3600
 
@@ -15,16 +15,22 @@ def conso(d, f, t = None):
   sprod = d[mask, 1]
   conso = d[mask, 2]
 
-  save = numpy.trapz(sprod - numpy.where(conso < 0, 0, conso), times) / 3600000
-  prod = numpy.trapz(sprod, times) / 3600000
-  edfs = numpy.trapz(numpy.where(conso < 0, -conso, 0), times) / 3600000
+  if partial or len(times) > 100:
+    save = numpy.trapz(sprod - numpy.where(conso < 0, 0, conso), times) / 3600000
+    prod = numpy.trapz(sprod, times) / 3600000
+    edfs = numpy.trapz(numpy.where(conso < 0, -conso, 0), times) / 3600000
+  else:
+    save = 0.
+    prod = 0.
+    edfs = 0.
 
   return save, prod, edfs
 
 if len(sys.argv) > 1:
   f = int(datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").timestamp())
-  print(conso(d, f)[int(sys.argv[2])])
+  print(conso(d, f, partial = True)[int(sys.argv[2])])
 else:
+  integral = [0., ] * 3
   s = datetime.datetime.fromtimestamp(d[0, 0], tz = datetime.timezone.utc)
   f = datetime.datetime(s.year, s.month, s.day + 1, tzinfo = datetime.timezone.utc).timestamp()
   while f < d[-1, 0] - 24 * 3600:
@@ -33,4 +39,8 @@ else:
       at = datetime.datetime.fromtimestamp(f, tz = datetime.timezone.utc)
       h = datetime.datetime(at.year, at.month, at.day, 22).timestamp()
       print(h, *vals)
+      integral[0] += vals[0]
+      integral[1] += vals[1]
+      integral[2] += vals[2]
     f += 24 * 3600
+  print("#", *integral)
